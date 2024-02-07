3e08b87e535cfb6d69b1e218d44fe5421e6e7483
Lucas Jiménez Núñez <ljimenez2nunez@gmail.com>
2024-02-07 18:18:59 +0100

    Reto #1 - Python and Java

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/LucasJimenezN.java
@@ -0,0 +1,18 @@
+public class Main(){
+    public static void main(String[] args){
+        for (int i = 0, i <101, i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("fizzbuzz")
+            }
+            else if(i % 3 == 0){
+                System.out.println("fizz")
+            }
+            else if(i % 5 == 0){
+                System.out.println("buzz")
+            }
+            else{
+                System.out.println(i)
+            }
+        }
+    }
+}
